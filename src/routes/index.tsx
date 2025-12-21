be998c0955517530f8ae0ceca1d821b55672892f
Dennis Gonzales <pro.gonzalesdennis@gmail.com>
2025-12-21 14:20:46 +0800

    ci: test cicd

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,5 +3,5 @@ import { createFileRoute } from "@tanstack/react-router";
 export const Route = createFileRoute("/")({ component: App });
 
 function App() {
-	return <h1 className="text-3xl ">Hello, World!!</h1>;
+	return <h1 className="text-3xl ">Hello, World!!!!1</h1>;
 }
